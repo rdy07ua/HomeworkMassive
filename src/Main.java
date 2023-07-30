d4133193c544f2cb702102107c5cfcfca67083fe
rdy07ua <spartakadoma.net@gmail.com>
2023-07-30 13:31:29 +0200

    Commit

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,56 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+import java.util.Random;
+
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
-
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
+        int[] myArray = new int[10]; // 10 numbers
+        Random random = new Random();
+        for (int i = 0; i < myArray.length; i++) {
+            myArray[i] = random.nextInt(100);
         }
+        for (int num : myArray) {
+            System.out.print(num + " ");
+        }
+        System.out.println(" ");
+
+
+        int minNumber = myArray[0]; // Min number
+        for (int i = 1; i < myArray.length; i++) {
+            if (myArray[i] < minNumber) {
+                minNumber = myArray[i];
+            }
+        }
+
+        System.out.println("The smallest number is " + minNumber);
+
+        int maxNumber = myArray[0]; // Max number
+        for (int i = 1; i < myArray.length; i++) {
+            if (myArray[i] > minNumber) {
+                maxNumber = myArray[i];
+            }
+        }
+        System.out.println("The largest number is " + maxNumber);
+
+        double sum = 0;   // Sum of the numbers Використовуємо дабл для більш точного обчислення середнього значення числа в подальному.
+        for (int num : myArray) {
+            sum += num;
+        }
+        System.out.println("The sum of the numbers is " + sum);
+
+        double average = sum / myArray.length; // the average number
+        System.out.println("The average number is " + average);
+
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
